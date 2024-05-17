634afc7b19ef458bbf0d08c16df8d281ae2c01da
cnkamorn <chanakan.amorn@gmail.com>
2024-05-17 16:21:44 +1000

    fixed unique value

--- backend/src/controllers/order.controller.js
+++ backend/src/controllers/order.controller.js
@@ -2,7 +2,9 @@ const db = require("../database");
 
 //get all orders
 exports.getAll = async (req, res) => {
-  const order = await db.order.findAll();
+  const order = await db.order.findAll({
+    include:{model: db.order_item, as:"order_item"}
+});
   res.json(order);
 };
 
--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -26,26 +26,31 @@ db.user.hasMany(db.order, { foreignKey: "user_id" });
 db.order.belongsTo(db.user, {
   foreignKey: { name: "user_id" },
   as: "user",
+  allownull: false
 });
-db.order_item.hasMany(db.order, { foreignKey: "order_item_id" });
+db.order_item.hasMany(db.order, { foreignKey: "order_item_id",unique: true });
 db.order.belongsTo(db.order_item, {
-  foreignKey: { name: "order_item_id" },
-  as: "order",
-});
+  foreignKey: { name: "order_item_id",unique: true  },
+  as: "order_item",
+  allownull: false
+}); 
 
 //Relate order items and product
 db.product.hasMany(db.order_item, { foreignKey: "product_id" });
 db.order_item.belongsTo(db.product, {
   foreignKey: { name: "product_id" },
+  allownull: false
 });
 //Relate cart and user
 db.user.hasMany(db.cart, { foreignKey: "user_id" });
 db.cart.belongsTo(db.user, {
   foreignKey: { name: "user_id" },
+  allownull: false
 });
 db.product.hasMany(db.cart, { foreignKey: "product_id" });
 db.cart.belongsTo(db.product, {
   foreignKey: { name: "product_id" },
+  allownull: false
 });
 // Relate special products and products | Special product table
 db.product.hasOne(db.special_product, {
@@ -56,20 +61,24 @@ db.user.belongsToMany(db.user, {
   through: db.follower,
   as: "user_follower",
   foreignKey: "user_followed",
+  allownull: false
 });
 db.user.belongsToMany(db.user, {
   through: db.follower,
   as: "user_followed",
   foreignKey: "user_follower",
+  allownull: false
 });
 //Relate review,product and user
 db.user.hasMany(db.review, { foreignKey: "user_id" });
 db.product.hasMany(db.review, { foreignKey: "product_id" });
 db.review.belongsTo(db.user, {
   foreignKey: { name: "user_id" },
+  allownull: false
 });
 db.review.belongsTo(db.product, {
   foreignKey: { name: "product_id" },
+  allownull: false
 });
 // Include a sync option with seed data logic included.
 db.sync = async () => {
