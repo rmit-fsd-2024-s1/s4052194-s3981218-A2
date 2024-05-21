1e9cf8dcb7fba0f82e34794f048c2aad03f05a0d
cnkamorn <chanakan.amorn@gmail.com>
2024-05-21 16:31:52 +1000

    added special products

--- backend/src/database/config.js
+++ backend/src/database/config.js
@@ -1,7 +1,7 @@
 module.exports = {
   HOST: "rmit.australiaeast.cloudapp.azure.com",
-  USER: "s3981218_fsd_a2",
-  PASSWORD: "Itsajoke..11",
-  DB: "s3981218_fsd_a2",
+  USER: "s4052194_fsd_a2",
+  PASSWORD: "password123",
+  DB: "s4052194_fsd_a2",
   DIALECT: "mysql"
 };
