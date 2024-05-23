d4e186caf61c85906dfe8a37132a05b7bf7d5c14
cnkamorn <chanakan.amorn@gmail.com>
2024-05-23 16:33:49 +1000

    updated review ui

--- frontend/src/components/Review.js
+++ frontend/src/components/Review.js
@@ -9,18 +9,37 @@ const Review = ({ productId }) => {
   const handleClick = () => {
     setShowInput(true);
   };
+
   //ref https://tutorial101.blogspot.com/2021/10/reactjs-star-rating.html
   //star rating
-  const [rating, setRating] = useState(1);
+  const [rating, setRating] = useState(5);
   const [hoverRating, setHoverRating] = useState(0);
   const stars = [1, 2, 3, 4, 5];
   const [comment, setComment] = useState();
-
-  useEffect(() => {
-    if (comment) {
-      console.log(comment.trim().split(" ").length);
+  const [warning, setWarning] = useState(false);
+  const [reply, showReply] = useState(false);
+  const [reviewReply, setReviewReply] = useState();
+  const [replyText, setReplyText] = useState();
+  const onSubmit = (parentId) => {
+    if (comment && comment.trim().split(" ").length > 100) {
+      console.log("exceeded 100 words");
+      setWarning(true);
+    } else {
+      setWarning(false);
+      console.log(
+        "user:",
+        userId,
+        "comment:",
+        comment,
+        "score:",
+        rating,
+        "product id:",
+        productId,
+        "parent id",
+        parentId
+      );
     }
-  }, [comment]);
+  };
 
   //loading reviews
   if (loadingReview) {
@@ -61,8 +80,13 @@ const Review = ({ productId }) => {
                 setComment(e.target.value);
               }}
             ></textarea>
-            {comment}
-            <button>submit</button>
+            {warning && (
+              <p className="text-danger">
+                Exceeded words limit. Maximum 100 words.{" "}
+              </p>
+            )}
+            {comment ? comment.trim().split(" ").length : 0} word(s)
+            <button onClick={onSubmit}>submit</button>
           </div>
         </>
       )}
@@ -71,15 +95,90 @@ const Review = ({ productId }) => {
       {reviews.map((review) => {
         return (
           <div>
-            <div class="card">
-              <div class="card-header bg-white">
-               <CommentStar rating={review.score}/>
+            {review.parent_id === null ? (
+              <div class="card p-3">
+                <div class="card-header bg-white">
+                  <CommentStar rating={review.score} />
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title fw-bold"> {review.user.username}</h5>
+                  <p class="card-text d-inline"> {review.comment}</p>
+                  {userId && (
+                    <div className="d-inline float-end">
+                      {" "}
+                      <span
+                        class="mx-5"
+                        onClick={() => {
+                          showReply(true);
+                          setReviewReply(review.review_id);
+                        }}
+                      >
+                        reply
+                      </span>
+                    </div>
+                  )}
+                  {/* show reply text field */}
+                  {reply && review.review_id === reviewReply && (
+                    <div className="mb-3">
+                      <label for="comment" className="form-label"></label>
+                      <textarea
+                        className="form-control"
+                        id="comment"
+                        rows="3"
+                        onChange={(e) => {
+                          setReplyText(e.target.value);
+                        }}
+                      ></textarea>
+                      {warning && (
+                        <p className="text-danger">
+                          Exceeded words limit. Maximum 100 words.{" "}
+                        </p>
+                      )}
+                      {replyText ? replyText.trim().split(" ").length : 0}{" "}
+                      word(s)
+                      <button onClick={() => onSubmit(review.review_id)}>
+                        submit
+                      </button>
+                    </div>
+                  )}
+                  {userId && review.user_id === userId ? (
+                    <div className="d-inline float-end">
+                      {" "}
+                      <span class="mx-5">edit</span>
+                      <span class="">delete</span>
+                    </div>
+                  ) : (
+                    ""
+                  )}
+                  {/* reply to review part */}
+                </div>
+
+                {reviews.map((r) => {
+                  if (r.parent_id === review.review_id) {
+                    return (
+                      <div class="ms-5">
+                        <div class="card-header bg-white"></div>
+                        <div class="card-body">
+                          <h5 class="card-title fw-bold"> {r.user.username}</h5>
+                          <p class="card-text"> {r.comment}</p>
+                          {userId && r.user_id === userId ? (
+                            <div className="d-inline float-end">
+                              {" "}
+                              <span class="mx-5">edit</span>
+                              <span class="">delete</span>
+                            </div>
+                          ) : (
+                            ""
+                          )}
+                        </div>
+                      </div>
+                    );
+                  }
+                })}
               </div>
-              <div class="card-body">
-                <h5 class="card-title fw-bold"> {review.user.username}</h5>
-                <p class="card-text"> {review.comment}</p>
-              </div>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
         );
       })}
@@ -115,27 +214,24 @@ const Star = ({ starId, rating, onMouseEnter, onMouseLeave, onClick }) => {
     </div>
   );
 };
-const CommentStar = ({rating}) => {
+const CommentStar = ({ rating }) => {
   const stars = [];
   for (let i = 0; i < rating; i++) {
     stars.push(
-    <svg
-      focusable="false"
-      viewBox="0 0 24 24"
-      width="24"
-      height="24"
-      aria-label=""
-      tabIndex="0"
-      aria-hidden="true"
-      class="ugc-rr-pip-fe-svg-icon ugc-rr-pip-fe-rating-star-bar__star ugc-rr-pip-fe-rating-star-bar__star--filled"
-    >
-      <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
-    </svg>);
+      <svg
+        focusable="false"
+        viewBox="0 0 24 24"
+        width="24"
+        height="24"
+        aria-label=""
+        tabIndex="0"
+        aria-hidden="true"
+        class="ugc-rr-pip-fe-svg-icon ugc-rr-pip-fe-rating-star-bar__star ugc-rr-pip-fe-rating-star-bar__star--filled"
+      >
+        <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
+      </svg>
+    );
   }
-  return (
-    <>
-      {stars}
-    </>
-  );
+  return <>{stars}</>;
 };
 export default Review;
