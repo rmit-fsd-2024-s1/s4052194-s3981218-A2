2830070753ee7dffda28abbb04ed47d576bfe226
cnkamorn <chanakan.amorn@gmail.com>
2024-05-30 02:13:54 +1000

    star

--- frontend/src/components/Star.js
+++ frontend/src/components/Star.js
@@ -20,7 +20,7 @@ const Star = ({ starId, rating, onMouseEnter, onMouseLeave, onClick }) => {
         aria-label=""
         tabIndex="0"
         aria-hidden="true"
-        className="ugc-rr-pip-fe-svg-icon ugc-rr-pip-fe-rating-star-bar__star ugc-rr-pip-fe-rating-star-bar__star--filled"
+        class="ugc-rr-pip-fe-svg-icon ugc-rr-pip-fe-rating-star-bar__star ugc-rr-pip-fe-rating-star-bar__star--filled"
       >
         <path d="m11.9999 6 2.1245 3.6818 4.1255.9018-2.8125 3.1773L15.8626 18l-3.8627-1.7182L8.1372 18l.4252-4.2391-2.8125-3.1773 4.1255-.9018L11.9999 6z"></path>
       </svg>
